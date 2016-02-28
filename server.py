35fc5fdeccbcc6096471f6a6e361f2d4ea601068
shawn <swanders@ualberta.ca>
2016-02-28 15:55:03 -0700

    passing tests - hitting recursion limit for some reason... ?

--- server.py
+++ server.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # 
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	 http://www.apache.org/licenses/LICENSE-2.0
 # 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,83 +18,94 @@
 # python server.py
 #
 # remember to:
-#     pip install flask
+#	 pip install flask
 
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
+from flask.ext.cors import CORS
 import json
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 
 # An example world
 # {
-#    'a':{'x':1, 'y':2},
-#    'b':{'x':2, 'y':3}
+#	'a':{'x':1, 'y':2},
+#	'b':{'x':2, 'y':3}
 # }
 
 class World:
-    def __init__(self):
-        self.clear()
-        
-    def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
-        entry[key] = value
-        self.space[entity] = entry
+	def __init__(self):
+		self.clear()
+		
+	def update(self, entity, key, value):
+		entry = self.space.get(entity,dict())
+		entry[key] = value
+		self.space[entity] = entry
 
-    def set(self, entity, data):
-        self.space[entity] = data
+	def set(self, entity, data):
+		self.space[entity] = data
 
-    def clear(self):
-        self.space = dict()
+	def clear(self):
+		self.space = dict()
 
-    def get(self, entity):
-        return self.space.get(entity,dict())
-    
-    def world(self):
-        return self.space
+	def get(self, entity):
+		return self.space.get(entity,dict())
+	
+	def world(self):
+		return self.space
 
 # you can test your webservice from the commandline
 # curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
 
-myWorld = World()          
+myWorld = World()		  
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
 def flask_post_json():
-    '''Ah the joys of frameworks! They do so much work for you
-       that they get in the way of sane operation!'''
-    if (request.json != None):
-        return request.json
-    elif (request.data != None and request.data != ''):
-        return json.loads(request.data)
-    else:
-        return json.loads(request.form.keys()[0])
+	'''Ah the joys of frameworks! They do so much work for you
+	   that they get in the way of sane operation!'''
+	if (request.json != None):
+		return request.json
+	elif (request.data != None and request.data != ''):
+		return json.loads(request.data)
+	else:
+		return json.loads(request.form.keys()[0])
 
 @app.route("/")
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+	'''Return something coherent here.. perhaps redirect to /static/index.html '''
+	return redirect('./static/index.html')
+
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+	'''update the entities via this interface'''
+	data = json.loads(request.data)
+	for i in data.items():
+		myWorld.update(entity, i[0], i[1])
+	e = json.dumps(myWorld.get(entity))
+	return e
 
-@app.route("/world", methods=['POST','GET'])    
+@app.route("/world", methods=['POST','GET'])	
 def world():
-    '''you should probably return the world here'''
-    return None
+	'''you should probably return the world here'''
+	w = json.dumps(myWorld.world())
+	return w
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")	
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+	'''This is the GET version of the entity interface, return a representation of the entity'''
+	e = json.dumps(myWorld.get(entity))
+	return e
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+	'''Clear the world out!'''
+	myWorld.clear();
+	w = json.dumps(myWorld.world())
+	return w
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
